e0d84b765f7de423ab0d8e64c6cdeaa623df57cc
James Wilson <macihenroomfiddling@gmail.com>
2022-04-02 14:06:52 +0100

    dosbox env ready to run imakef

--- db.sh
+++ db.sh
@@ -1,1 +1,6 @@
-dosbox -c "mount D /home/james" -c "mount E /home/james/d7205a/install" -c "PATH=Z:;E:\D7205\ITOOLS"
+dosbox -c "mount D /home/james"\
+       -c "mount E /home/james/d7205a/install"\
+       -c "PATH=Z:;E:\D7205\ITOOLS;e:\d7205\iserver"\
+       -c "D:" -c "cd /occam-~1/raytra~1"
+
+#       -c "set ISERVER=ISVRB08.EXE"\
